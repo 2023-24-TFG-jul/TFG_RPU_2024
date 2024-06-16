bc590ba204e52e682c0978c0eff2e23d9bb250e1
RodrigoPerezUbierna <rpu1001@alu.ubu.es>
2024-06-16 22:46:04 +0200

    Mejora de los mensajes

--- appRPU/kairos/lib/views/view_auctions.dart
+++ appRPU/kairos/lib/views/view_auctions.dart
@@ -63,9 +63,7 @@ class _ViewAuctionsState extends State<ViewAuctions> {
       await _auctionRepository.deleteAuction(id);
       _loadAuctions(loginUserEmail);
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error deleting the auction: $e')),
-      );
+      _showDialog('Error', 'Error deleting auction: $e');
     }
   }
 
@@ -83,14 +81,10 @@ class _ViewAuctionsState extends State<ViewAuctions> {
       await _userRepository.updateUserWallet(
           auction.vendorEmail, -auction.maximumValue); //para que sume
       _loadAuctions(loginUserEmail);
-      Navigator.pop(context, true);
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Watch successfully acquired.')),
-      );
+      _showDialog('Correct purchase', 'Watch successfully acquired.');
+      return;
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error when purchasing the watch: $e')),
-      );
+      _showDialog('Bad purchase', 'Error when purchasing the watch: $e');
     }
   }
 
@@ -122,27 +116,18 @@ class _ViewAuctionsState extends State<ViewAuctions> {
                 if (bidAmount != 0) {
                   int newBid = bidAmount;
                   if (newBid >= auction.maximumValue) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content:
-                              Text('You cannot exceed the maximum value.')),
-                    );
+                    _showDialog('Invalid data', 'You cannot exceed the maximum value.');
+                    return;
                   } else if (newBid > auction.actualValue &&
                       newBid >= auction.minimumValue) {
                     await _auctionRepository.updateBid(
                         auction.watchNickName, newBid, loginUserEmail);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                          content:
-                              Text('Bid of $bidAmount € placed successfully.')),
-                    );
+                    _showDialog('Correct bid', 'Bid of $bidAmount € placed successfully.');
                     _loadAuctions(loginUserEmail);
+                    return;
                   } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                          content:
-                              Text('Bid must be higher than current value.')),
-                    );
+                    _showDialog('Invalid data', 'Bid must be higher than current value.');
+                    return;
                   }
                 }
                 Navigator.of(context).pop();
@@ -166,88 +151,94 @@ class _ViewAuctionsState extends State<ViewAuctions> {
       appBar: AppBar(
         title: const Text('Auctions'),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/kairos_wallpaper.png'),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/kairos_wallpaper.png',
             fit: BoxFit.cover,
           ),
-        ),
-        child: FutureBuilder<List<Auction>>(
-          future: _auctionsFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(child: Text('No auctions found.'));
-            } else {
-              List<Auction> auctions = snapshot.data!;
-              return ListView.builder(
-                itemCount: auctions.length,
-                itemBuilder: (context, index) {
-                  Auction auction = auctions[index];
-                  bool isVendor = auction.vendorEmail == loginUserEmail;
-                  bool isExpired = _isAuctionExpired(auction);
+          Container(
+            color: Colors.black.withOpacity(0.4),
+          ),
+          Center(
+            child: FutureBuilder<List<Auction>>(
+              future: _auctionsFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No auctions found.'));
+                } else {
+                  List<Auction> auctions = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: auctions.length,
+                    itemBuilder: (context, index) {
+                      Auction auction = auctions[index];
+                      bool isVendor = auction.vendorEmail == loginUserEmail;
+                      bool isExpired = _isAuctionExpired(auction);
 
-                  return Card(
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    child: ListTile(
-                      title: Text(auction.watchNickName),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          _buildRichText('Vendor:\n', auction.vendorEmail),
-                          _buildRichText('Buyer:\n', auction.buyerEmail),
-                          _buildRichText('Start Value:\n',
-                              auction.minimumValue.toString()),
-                          _buildRichText('Actual Value:\n',
-                              auction.actualValue.toString()),
-                          _buildRichText('Direct Sale Price:\n',
-                              auction.maximumValue.toString()),
-                          _buildRichText(
-                              'Limit Date:\n', auction.limitDate.toString()),
-                          _buildRichText('Status:\n', auction.auctionStatus),
-                        ],
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: isVendor &&
-                                    !isExpired &&
-                                    auction.auctionStatus != 'Finished'
-                                ? () => _deleteAuction(auction.idAuction)
-                                : null,
+                      return Card(
+                        margin:
+                            const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                        child: ListTile(
+                          title: Text(auction.watchNickName),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              _buildRichText('Vendor:\n', auction.vendorEmail),
+                              _buildRichText('Buyer:\n', auction.buyerEmail),
+                              _buildRichText('Start Value:\n',
+                                  auction.minimumValue.toString()),
+                              _buildRichText('Actual Value:\n',
+                                  auction.actualValue.toString()),
+                              _buildRichText('Direct Sale Price:\n',
+                                  auction.maximumValue.toString()),
+                              _buildRichText(
+                                  'Limit Date:\n', auction.limitDate.toString()),
+                              _buildRichText('Status:\n', auction.auctionStatus),
+                            ],
                           ),
-                          IconButton(
-                            icon: const Icon(Icons.shopping_cart),
-                            onPressed: !isVendor &&
-                                    !isExpired &&
-                                    auction.auctionStatus != 'Finished'
-                                ? () => _buyAuction(auction.idAuction)
-                                : null,
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: isVendor &&
+                                        !isExpired &&
+                                        auction.auctionStatus != 'Finished'
+                                    ? () => _deleteAuction(auction.idAuction)
+                                    : null,
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.shopping_cart),
+                                onPressed: !isVendor &&
+                                        !isExpired &&
+                                        auction.auctionStatus != 'Finished'
+                                    ? () => _buyAuction(auction.idAuction)
+                                    : null,
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.euro),
+                                onPressed: !isVendor &&
+                                        !isExpired &&
+                                        auction.auctionStatus != 'Finished'
+                                    ? () => _showBidDialog(auction)
+                                    : null,
+                              ),
+                            ],
                           ),
-                          IconButton(
-                            icon: const Icon(Icons.euro),
-                            onPressed: !isVendor &&
-                                    !isExpired &&
-                                    auction.auctionStatus != 'Finished'
-                                ? () => _showBidDialog(auction)
-                                : null,
-                          ),
-                        ],
-                      ),
-                    ),
+                        ),
+                      );
+                    },
                   );
-                },
-              );
-            }
-          },
-        ),
+                }
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _navigateToAddAuction,
@@ -271,6 +262,27 @@ class _ViewAuctionsState extends State<ViewAuctions> {
           ),
         ],
       ),
+    );
+  }
+
+    // Function defining the warning format of error messages.
+  void _showDialog(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
